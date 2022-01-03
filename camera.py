f68991067e09abaca9f74d817b4ce67ad3d0ac9d
cesar <relven@protonmail.com>
2022-01-03 17:29:09 -0600

    Se agregó soporte para dos camaras, webcam y PSEye en el puerto 2

--- camera.py
+++ camera.py
@@ -2,16 +2,23 @@ import cv2
 import numpy as np
 
 cap = cv2.VideoCapture(2)
+cap1 = cv2.VideoCapture(0)
 
 while(True):
     ret, frame = cap.read()
+    ret1, frame1 = cap1.read()
 
     if not ret:
         break
 
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    if not ret1:
+        break
 
-    cv2.imshow('frame', frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('frame', gray)
+    cv2.imshow('frame1', gray1)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
